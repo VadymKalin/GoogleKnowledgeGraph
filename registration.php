a185f0967242a62a227cb8856be0c9d3024a8f21
Vadym Kalin <kalinvadim@gmail.com>
2016-11-13 18:00:02 +0200

    Changed copyright

--- registration.php
+++ registration.php
@@ -1,6 +1,9 @@
 <?php
 /**
- * Copyright © 2016 Magento. All rights reserved.
+ * Copyright © 2016 Vadym Kalin.
+ * http://full-stack-dev.com
+ * kalinvadim@gmail.com
+ * All rights reserved.
  * See COPYING.txt for license details.
  */
 
